7c15e2c820c88dbf0a0e1ae344ab52aa573b2d79
Tetiana Vozniuk <taniavoznyk19655@gmail.com>
2025-01-23 14:39:30 +0200

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.scss';
 
 function getRandomName(): string {
@@ -7,31 +7,74 @@ function getRandomName(): string {
   return `Clock-${value}`;
 }
 
-export const App: React.FC = () => {
-  const today = new Date();
-  let clockName = 'Clock-0';
+type State = {
+  clockName: string;
+  time: string;
+}
 
-  // This code starts a timer
-  const timerId = window.setInterval(() => {
-    clockName = getRandomName();
-  }, 3300);
+export class App extends React.Component<{}, State> {
+  state: State = {
+    clockName: getRandomName(),
+    time: new Date().toUTCString().slice(-12, -4)
+  }
 
-  // this code stops the timer
-  window.clearInterval(timerId);
+  render() {
 
-  return (
-    <div className="App">
-      <h1>React clock</h1>
+    return (
+      <div className="App">
+        <h1>React clock</h1>
 
-      <div className="Clock">
-        <strong className="Clock__name">{clockName}</strong>
+        <div className="Clock">
+          <strong className="Clock__name">{clockName}</strong>
 
-        {' time is '}
+          {' time is '}
 
-        <span className="Clock__time">
-          {today.toUTCString().slice(-12, -4)}
-        </span>
+          <span className="Clock__time">{time}</span>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
+  // const [clockName, setClockName] = useState(getRandomName());
+  // const [time, setTime] = useState(new Date().toUTCString().slice(-12, -4));
+
+  useEffect(() => {
+    const handleDocuentRightClick = (event: MouseEvent) => {
+      // this.setState({ value: event.key  })
+      // eslint-disable-next-line no-console
+      console.warn(event);
+    };
+
+    const timeId = setInterval(() => {
+      setClockName(getRandomName());
+      setTime(new Date().toUTCString().slice(-12, -4));
+    }, 3300);
+
+    document.addEventListener('contextmenu', handleDocuentRightClick);
+
+    // eslint-disable-next-line no-console
+    console.log('compometDidMout');
+
+    // return () => clearInterval(timeId);
+    return () => {
+      window.clearInterval(timeId);
+      document.removeEventListener('contextmenu', handleDocuentRightClick);
+      console.log('componentWillUnmount');
+    };
+  }, []);
+
+  // componentDidMount(): void {
+  //   document.addEve
+  // }
+  // const today = new Date();
+  // let clockName = 'Clock-0';
+
+  // // This code starts a timer
+  // const timerId = window.setInterval(() => {
+  //   clockName = getRandomName();
+  // }, 3300);
+
+  // // this code stops the timer
+  // window.clearInterval(timerId);
+
+
 };
